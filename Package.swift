45977e2932c4d62d1896d2660d8bd6a1fb3bb550
Thomas De Leon <3507743+tdeleon@users.noreply.github.com>
2023-01-23 19:47:28 -0800

    Package platform macOS

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .iOS(.v13),
         .tvOS(.v13),
         .watchOS(.v6),
-        .macOS(.v13),
+        .macOS(.v12),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
