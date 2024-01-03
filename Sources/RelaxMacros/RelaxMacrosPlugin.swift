65081894dfd886b462359f349bb006309b88b8f7
Thomas De Leon <3507743+tdeleon@users.noreply.github.com>
2024-01-03 14:42:44 -0800

    #35: Remove  unnecessary import check

--- Sources/RelaxMacros/RelaxMacrosPlugin.swift
+++ Sources/RelaxMacros/RelaxMacrosPlugin.swift
@@ -8,12 +8,9 @@
 import SwiftCompilerPlugin
 import SwiftSyntaxMacros
 
-#if canImport(XCTest) && os(Windows) // temporarily disable when running tests on windows https://github.com/apple/swift/issues/69302
-#else
 @main
 struct RelaxMacrosPlugin: CompilerPlugin {
     let providingMacros: [Macro.Type] = [
         APIServiceMacro.self
     ]
 }
-#endif
