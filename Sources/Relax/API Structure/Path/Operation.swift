6666e51ecedbfd3060eec04f5c12d85a095697c3
Thomas De Leon <3507743+tdeleon@users.noreply.github.com>
2026-05-21 15:22:18 -0700

    #74: Operation DSL type

--- Sources/Relax/API Structure/Path/Operation.swift
+++ Sources/Relax/API Structure/Path/Operation.swift
@@ -16,7 +16,7 @@ extension Path {
         public let description: String?
         public let tags: [Tag]
         public let parameters: [Parameter]
-        public let responses: [Response.ResponseType: Response]
+        public let responses: [Response]
         public let security: [SecurityScheme]
         public let servers: [Server]
         
@@ -24,7 +24,7 @@ extension Path {
             _ method: HTTPRequest.Method,
             id: StaticString? = nil,
             summary: String? = nil,
-            @ResponsesBuilder responses: () -> [Response.ResponseType : Response],
+            @ResponsesBuilder responses: () -> [Response],
             @Parameter.Builder parameters: () -> [Parameter] = { [] },
             @Tag.Builder tags: () -> [Tag] = { [] },
             @SecurityBuilder security: () -> [SecurityScheme] = { [] },
@@ -44,25 +44,25 @@ extension Path {
         
         @resultBuilder
         public enum ResponsesBuilder {
-            public static func buildBlock() -> [Response.ResponseType: Response] {
-                [:]
+            public static func buildBlock() -> [Response] {
+                []
             }
             
             public static func buildPartialBlock(
-                first: [Response.ResponseType : Response]
-            ) -> [Response.ResponseType : Response] {
+                first: [Response]
+            ) -> [Response] {
                 first
             }
             
             public static func buildPartialBlock(
-                accumulated: [Response.ResponseType : Response],
-                next: [Response.ResponseType : Response]
-            ) -> [Response.ResponseType : Response] {
-                accumulated.merging(next) { _, new in new }
+                accumulated: [Response],
+                next: [Response]
+            ) -> [Response] {
+                accumulated + next
             }
             
-            public static func buildExpression(_ expression: Response) -> [Response.ResponseType : Response] {
-                [expression.responseType: expression]
+            public static func buildExpression(_ expression: Response) -> [Response] {
+                [expression]
             }
         }
     }
